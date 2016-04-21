8097900751e1bdada27c546059f27dd347b4a3d6
doutib <thibault.doutre@gmail.com>
2016-04-20 18:18:07 -0700

    More travis bugs

--- mousestyles/density_estimation.py
+++ mousestyles/density_estimation.py
@@ -5,14 +5,6 @@ import numpy as np
 import pandas as pd
 from math import sqrt
 
-# # Extract distances
-# Here are a bunch of functions for extracting distances with a given delta t 
-# (step) which corresponds to the interval of time used for the data 
-# to be aggregated.
-# ## Strain, mouse, day
-# Extract the distance distribution for a particular mouse of a given strain 
-# at a particular day.
-
 
 def extract_distances(strain, mouse, day, step=1e2):
     """
@@ -40,7 +32,7 @@ def extract_distances(strain, mouse, day, step=1e2):
     movement = load_movement(strain, mouse, day)
     # Compute distance between samples
     dist = np.empty((movement.shape[0], 2))
-    dist[0,0] = sqrt(movement.iloc[0, 1]*movement.iloc[0, 1]+
+    dist[0, 0] = sqrt(movement.iloc[0, 1]*movement.iloc[0, 1]+
                      movement.iloc[0, 2]*movement.iloc[0, 2])
     for i in range(1, len(dist[:, 0])):
         x1 = movement.iloc[i-1, 1]
@@ -50,7 +42,7 @@ def extract_distances(strain, mouse, day, step=1e2):
         x = x2 - x1
         y = y2 - y1
         d = sqrt(x*x+y*y)
-        dist[i,0] = d
+        dist[i, 0] = d
     dist[:, 1] = np.array(movement['t'])
     dist[:, 1] = dist[:, 1] - dist[0, 1]
     tf = dist[dist.shape[0]-1, 1]
@@ -58,15 +50,10 @@ def extract_distances(strain, mouse, day, step=1e2):
     aggregate = np.zeros(int(tf/step))
     j = 0
     for i in range(len(aggregate)):
-        while dist[j, 1]<i*step:
+        while dist[j, 1] < i*step:
             aggregate[i] = aggregate[i] + dist[j, 0]
             j = j+1
     return(aggregate)
-
-
-# ## Strain, mouse
-# Extract the distance distribution for a particular mouse of a given
-# strain for all days.
 
 
 def extract_distances_bymouse(strain, mouse, step=1e2, verbose=False):
@@ -125,7 +112,7 @@ def extract_distances_bystrain(strain, step=1e2, verbose=False):
     mouse = 0
     res = []
     e = np.array([0])
-    while e.size>0:
+    while e.size > 0:
         e = extract_distances_bymouse(strain, mouse, step=step)
         res.append(list(e))
         mouse = mouse + 1
